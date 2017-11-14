2bca72284274f03cfa263627e087df377e464b66
hissingshark <git@hissingshark.co.uk>
2017-11-14 20:18:01 +0000

    Create sdl2_build.sh

---
+++ sdl2_build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# *************************************************
+# Script to build SDL2 on the OSMC Vero 4K
+# HissingShark 2017 -
+# *************************************************
+
+# stop Kodi to free up resources
+sudo systemctl stop mediacenter
+
+# install typical building suite
+sudo apt-get update
+sudo apt-get install -y vero3-userland-dev-osmc build-essential git
+
+# set flags useful to all of the builds on our Amlogic S905x based Vero 4K
+export CFLAGS="-I/opt/vero3/include -L/opt/vero3/lib -O3 -march=armv8-a+crc -mtune=cortex-a53 -funsafe-math-optimizations"
+export CPPFLAGS=$CFLAGS
+export CXXFLAGS=$CFLAGS
+
+# install specific packages for building  SDL2
+sudo apt-get install -y libasound2-dev libdbus-1-dev
+
+# pull a fork of SDL2 v.2.0.2 that includes MALI-fbdev support
+cd
+git clone https://github.com/mihailescu2m/libsdl2-2.0.2-dfsg1.git
+
+# build/install SDL2 - a major dependancy of the project
+cd libsdl2-2.0.2-dfsg1
+./configure --enable-video-directfb
+sudo make -j4
+sudo make install
+cd
